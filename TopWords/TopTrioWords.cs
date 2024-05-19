fc71fc42e598ce5a9a4f8e556a8d73f6d3916905
Adron Hall <adron@thrashingcode.com>
2024-05-18 20:30:19 -0700

    Just some pedantic clean up.

---
+++ TopWords/TopTrioWords.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.RegularExpressions;
+
+namespace TopWords;
+
+public static class TopTrioWords
+{
+    public static List<string> Top3(string s)
+    {
+        // Remove all non-letter characters except apostrophes
+        string cleanedText = Regex.Replace(s, "[^a-zA-Z']", " ");
+
+        // Split the cleaned text into words
+        string[] words = cleanedText.ToLower().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+        // Count the occurrences of each word
+        Dictionary<string, int> wordCounts = new Dictionary<string, int>();
+        foreach (string word in words)
+        {
+            if (string.IsNullOrWhiteSpace(word) || Regex.IsMatch(word, "^'+$"))
+            {
+                continue;
+            }
+
+            if (wordCounts.ContainsKey(word))
+            {
+                wordCounts[word]++;
+            }
+            else
+            {
+                wordCounts[word] = 1;
+            }
+        }
+
+        // Get the top 3 words by occurrence count
+        List<string> topWords = wordCounts
+            .OrderByDescending(x => x.Value)
+            .ThenBy(x => x.Key)
+            .Take(3)
+            .Select(x => x.Key)
+            .ToList();
+
+        return topWords;
+    }
+}
--- TopWordsTests/SampleTests.cs
+++ TopWordsTests/SampleTests.cs
@@ -1,3 +1,5 @@
+using TopWords;
+
 namespace TopWordsTests;
 
 public class Tests
@@ -11,19 +13,21 @@ public class Tests
     [Test]
     public void SampleTests()
     {
-        Assert.That(TopWords.TopWords.Top3("a a a b c c d d d d e e e e e"), Is.EqualTo(new List<string> { "e", "d", "a" }));
-        Assert.That(TopWords.TopWords.Top3("e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e"), Is.EqualTo(new List<string> { "e", "ddd", "aa" }));
-        Assert.That(TopWords.TopWords.Top3(" //wont won't won't "), Is.EqualTo(new List<string> { "won't", "wont" }));
-        Assert.That(TopWords.TopWords.Top3(" , e .. "), Is.EqualTo(new List<string> { "e" }));
-        Assert.That(TopWords.TopWords.Top3(" ... "), Is.EqualTo(new List<string> { }));
-        Assert.That(TopWords.TopWords.Top3(" ' "), Is.EqualTo(new List<string> { }));
-        Assert.That(TopWords.TopWords.Top3(" ''' "), Is.EqualTo(new List<string> { }));
-        Assert.That(TopWords.TopWords.Top3(
-            string.Join("\n", new string[]{"In a village of La Mancha, the name of which I have no desire to call to",
+        Assert.That(TopTrioWords.Top3("a a a b c c d d d d e e e e e"), Is.EqualTo(new List<string> { "e", "d", "a" }));
+        Assert.That(TopTrioWords.Top3("e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e"), Is.EqualTo(new List<string> { "e", "ddd", "aa" }));
+        Assert.That(TopTrioWords.Top3(" //wont won't won't "), Is.EqualTo(new List<string> { "won't", "wont" }));
+        Assert.That(TopTrioWords.Top3(" , e .. "), Is.EqualTo(new List<string> { "e" }));
+        Assert.That(TopTrioWords.Top3(" ... "), Is.EqualTo(new List<string>()));
+        Assert.That(TopTrioWords.Top3(" ' "), Is.EqualTo(new List<string>()));
+        Assert.That(TopTrioWords.Top3(" ''' "), Is.EqualTo(new List<string>()));
+        Assert.That(TopTrioWords.Top3(
+            string.Join("\n", [
+                "In a village of La Mancha, the name of which I have no desire to call to",
                 "mind, there lived not long since one of those gentlemen that keep a lance",
                 "in the lance-rack, an old buckler, a lean hack, and a greyhound for",
                 "coursing. An olla of rather more beef than mutton, a salad on most",
                 "nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra",
-                "on Sundays, made away with three-quarters of his income." })), Is.EqualTo(new List<string> { "a", "of", "on" }));
+                "on Sundays, made away with three-quarters of his income."
+            ])), Is.EqualTo(new List<string> { "a", "of", "on" }));
     }
 }
